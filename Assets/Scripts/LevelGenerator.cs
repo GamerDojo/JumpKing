a50213c8f52f94d5fb4d198ac6f6129e0fc2efaf
Gabri127 <75862004+Gabriel1961@users.noreply.github.com>
2023-10-21 13:02:29 +0300

    added platform generation and jump direction

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject platformPrefab;
+    [SerializeField] float maxYDistance = 8;
+    [SerializeField] float minYDistance = 4;
+    [SerializeField] float maxXDistance = 8;
+    [SerializeField] float minXDistance = -8;
+    [SerializeField] int nrOfPlatforms = 20;
+    private void GenerateLevel()
+    {
+        float currentY = 0;
+        for(int i = 0; i < nrOfPlatforms; i++)
+        {
+            currentY += Random.Range(minYDistance, maxYDistance);
+            float currentX;
+            if (i % 2 == 0) // daca i este par 
+            {
+                currentX =-Random.Range(minXDistance, maxXDistance);
+            }
+            else // daca i este impar 
+            {
+                currentX = Random.Range(minXDistance, maxXDistance);
+            }
+            Instantiate(platformPrefab,new Vector3(currentX,currentY,0),Quaternion.identity);
+        }
+    }
+
+    private void Start()
+    {
+        GenerateLevel();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 
 using System;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,13 +10,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Transform groundCheck;
     [SerializeField] float jumpForce = 10;
     [SerializeField] float chargeRate = 0.3f; // per second 
+    [SerializeField] float deathY = -10; // height at which player dies
+    
+    Vector3 initialPosition; // this is used for respawning 
+
+    Camera mainCamera;
     float jumpCharge;
     bool isGrounded;
 
     private void GroundCheck()
     {
         int excludePlayer = ~0 - LayerMask.GetMask("Player");
-        var hit = Physics2D.BoxCast(groundCheck.transform.position, new Vector2(0.001f, 0.001f), 0, Vector2.down, 0.15f, excludePlayer);
+        var hit = Physics2D.BoxCast(groundCheck.transform.position, new Vector2(1.1f, 1), 0, Vector2.down, 0.15f, excludePlayer);
         if (hit)
         {
             isGrounded = true;
@@ -26,9 +32,11 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+        initialPosition = transform.position;
     }
-
+    private float zoomVel;
     private void Update()
     {
         if (Input.GetMouseButton(0))
@@ -45,17 +53,30 @@ public class PlayerController : MonoBehaviour
 
         // ground check 
         GroundCheck();
-        if(isGrounded==false)
+        if (isGrounded == false)
             jumpCharge = 0;
 
         // set chargebar ui
         var scale = chargeBar.localScale;
         scale.x = jumpCharge;
         chargeBar.localScale = scale;
+
+        // respawn 
+        if(transform.position.y < deathY)
+        {
+            transform.position = initialPosition;
+        }
+
+        mainCamera.orthographicSize = Mathf.SmoothDamp(mainCamera.orthographicSize, Mathf.Lerp(5, 10, rb.velocity.magnitude / jumpForce), ref zoomVel, .2f);
     }
 
     private void Jump()
     {
-        rb.AddForce(Vector2.up * jumpForce * jumpCharge, ForceMode2D.Impulse);
-    }
+        var mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        var dif = mousePos - transform.position;
+        dif.z = 0; 
+
+        rb.AddForce(dif.normalized * jumpForce * jumpCharge, ForceMode2D.Impulse);
+        jumpCharge = 0;
+    } 
 }
